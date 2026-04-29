894a87e98babae7b546efd5b6e588919eeaf9992
EaeDave <d4vd.sv@gmail.com>
2026-04-29 18:41:49 -0300

    pacotes

---
+++ 1 - Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// Essa função está no escopo do pacote auxiliar, e somente é acessado por ele.
+func escrever2() {
+	fmt.Println("Escrevendo pelo pacote auxiliar2")
+}
