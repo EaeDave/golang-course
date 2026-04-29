6c0f7262278a349d10a33a9c0e034432eb5753d3
EaeDave <d4vd.sv@gmail.com>
2026-04-29 18:54:08 -0300

    pacotes externos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main.")
 	auxiliar.Escrever()
+	emailValido := checkmail.ValidateFormat("devbook@gmail.com")
+	emailInvalido := checkmail.ValidateFormat("123")
+
+	fmt.Println((emailValido))
+	fmt.Println(emailInvalido)
 }
